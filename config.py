cee2e9b7d9dfe13bc9095fecf65a5297bb2ec582
Jimy dc <Jimyvdachi@gmail.com>
2026-02-04 20:40:47 +0700

    redesign ui

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ class Config:
     DIFFICULTY = 4
     MINING_REWARD = 50
     BOOTSTRAP_NODES = [
-        "http://localhost:5000",
-        "http://localhost:5001",
-        "http://localhost:5002",
-        "http://localhost:5003",
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:3002",
     ]
